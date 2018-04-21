7f12af2b44daa88cda4def9c8a3705f3378993bc
Enrique <enri46@gmail.com>
2018-04-21 02:21:14 +0200

    Página de Login y su ViewModel

---
+++ AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/Features/Login/LoginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using AppCursoXamarinDevsDNA.Services.NavigationService;
+using ReactiveUI;
+using System;
+using Xamarin.Forms.Xaml;
+
+namespace AppCursoXamarinDevsDNA.Features.Login
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage
+	{
+		public LoginPage(NavigationParameters navigationParameters = null) : base(navigationParameters)
+        {
+            InitializeComponent();
+        }
+
+        public override void CreateBindings(Action<IDisposable> d)
+        {
+            base.CreateBindings(d);
+
+            d(this.OneWayBind(ViewModel, vm => vm.IsBusy, v => v.entryUser.IsEnabled, (isEnabled) => !isEnabled));
+            d(this.OneWayBind(ViewModel, vm => vm.IsBusy, v => v.entryPassword.IsEnabled, (isEnabled) => !isEnabled));
+            d(this.OneWayBind(ViewModel, vm => vm.IsLoginErrorLabelVisible, v => v.labelWrongUserOrPass.IsVisibleCustom));
+            d(this.OneWayBind(ViewModel, vm => vm.IsBusy, v => v.activityIndicator.IsRunning));
+            d(this.OneWayBind(ViewModel, vm => vm.IsBusy, v => v.buttonLogin.IsEnabled, (isEnabled) => !isEnabled));
+            d(this.BindCommand(ViewModel, vm => vm.LoginButtonCommand, v => v.buttonLogin));
+        }
+    }
+}
---
+++ AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/Features/Login/LoginPageViewModel.cs
@@ -0,0 +1,63 @@
+﻿using AppCursoXamarinDevsDNA.Base;
+using AppCursoXamarinDevsDNA.Services.Analytics;
+using AppCursoXamarinDevsDNA.Services.NavigationService;
+using ReactiveUI;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AppCursoXamarinDevsDNA.Features.Login
+{
+    public class LoginPageViewModel : BaseViewModel
+    {
+        private ReactiveCommand _loginButtonCommand;
+        public ReactiveCommand LoginButtonCommand => _loginButtonCommand;
+
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set => this.RaiseAndSetIfChanged(ref _email, value);
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set => this.RaiseAndSetIfChanged(ref _password, value);
+        }
+
+        private bool _isLoginErrorLabelVisible;
+        public bool IsLoginErrorLabelVisible
+        {
+            get => _isLoginErrorLabelVisible;
+            set => this.RaiseAndSetIfChanged(ref _isLoginErrorLabelVisible, value);
+        }
+
+        public LoginPageViewModel()
+        {
+            _loginButtonCommand = ReactiveCommand.CreateFromTask(LoginButtonTappedAsync);
+        }
+
+        public override void Load(NavigationParameters navigationParameters)
+        {
+            base.Load(navigationParameters);
+
+            //
+        }
+
+        private async Task LoginButtonTappedAsync()
+        {
+            IsBusy = true;
+
+            //TODO: Hacer un check falso del email y password, y guardar email como token de inicio de sesión
+            //Simular petición de Login
+            await Task.Delay(3000);
+
+            AnalyticsService.TrackEvent(AppCenterEvents.USER_LOG_IN, new Dictionary<string, string> {
+                        { "userId", Email }
+                  });
+
+            NavigationService.SetMainPage(new MainPage());
+        }
+    }
+}
