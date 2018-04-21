395319dcdce2f1702ca08f355286c91812ab9a51
Enrique <enri46@gmail.com>
2018-04-21 02:18:17 +0200

    BaseViewModel actualizado

--- AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/Base/BaseViewModel.cs
+++ AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/Base/BaseViewModel.cs
@@ -1,4 +1,6 @@
-﻿using AppCursoXamarinDevsDNA.Services.NavigationService;
+﻿using AppCursoXamarinDevsDNA.Services.Analytics;
+using AppCursoXamarinDevsDNA.Services.AppProperties;
+using AppCursoXamarinDevsDNA.Services.NavigationService;
 using ReactiveUI;
 using Splat;
 
@@ -7,14 +9,36 @@ namespace AppCursoXamarinDevsDNA.Base
     public abstract class BaseViewModel : ReactiveObject
     {
         private readonly INavigationService _navigationService;
+        private readonly IAnalyticsService _analyticsService;
+        private readonly IAppPropertiesService _appPropertiesService;
+
         public INavigationService NavigationService
         {
             get => _navigationService;
         }
 
-        public BaseViewModel(INavigationService navigationService = null)
+        public IAnalyticsService AnalyticsService
+        {
+            get => _analyticsService;
+        }
+
+        public IAppPropertiesService AppPropertiesService
+        {
+            get => _appPropertiesService;
+        }
+
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set { this.RaiseAndSetIfChanged(ref _isBusy, value); }
+        }
+
+        public BaseViewModel(INavigationService navigationService = null, IAnalyticsService analyticsService = null, IAppPropertiesService appPropertiesService = null)
         {
             _navigationService = navigationService ?? Locator.Current.GetService<INavigationService>();
+            _analyticsService = analyticsService ?? Locator.Current.GetService<IAnalyticsService>();
+            _appPropertiesService = appPropertiesService ?? Locator.Current.GetService<IAppPropertiesService>();
         }
 
         public virtual void Load(NavigationParameters navigationParameters)
