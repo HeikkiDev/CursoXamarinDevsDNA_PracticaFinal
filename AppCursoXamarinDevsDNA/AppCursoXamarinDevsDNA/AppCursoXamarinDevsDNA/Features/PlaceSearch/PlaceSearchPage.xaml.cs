6fe47532228112e38be364b05a1731fa4854115a
Enrique <enri46@gmail.com>
2018-04-22 14:58:55 +0200

    Nueva página PlaceSearchPage con la barra de búsqueda de direcciones y listado de resultados sugeridos para seleccionar

---
+++ AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/Features/PlaceSearch/PlaceSearchPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AppCursoXamarinDevsDNA.Services.NavigationService;
+using ReactiveUI;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCursoXamarinDevsDNA.Features.PlaceSearch
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlaceSearchPage
+    {
+        public PlaceSearchPage(NavigationParameters navigationParameters = null) : base(navigationParameters)
+        {
+            InitializeComponent();
+        }
+
+        public override void CreateBindings(Action<IDisposable> d)
+        {
+            base.CreateBindings(d);
+
+            d(this.OneWayBind(ViewModel, vm => vm.AutoCompletePlacesCommand, v => v.autocompleteSearchBar.AutocompleteCommand));
+
+            d(this.OneWayBind(ViewModel, vm => vm.PlacesList, v => v.listViewPlacesAutocomplete.ItemsSource));
+            d(this.Bind(ViewModel, vm => vm.SelectedItem, v => v.listViewPlacesAutocomplete.SelectedItem));
+
+            // TODO: Ver cómo hacer el Binding del Label que está dentro del ListView
+
+            //Focus a la barra de búsqueda
+            autocompleteSearchBar.Focus();
+        }
+    }
+}
---
+++ AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/Features/PlaceSearch/PlaceSearchPageViewModel.cs
@@ -0,0 +1,88 @@
+﻿using AppCursoXamarinDevsDNA.Base;
+using AppCursoXamarinDevsDNA.Services.NavigationService;
+using AppCursoXamarinDevsDNA.Services.PlaceAutocomplete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppCursoXamarinDevsDNA.Features.PlaceSearch
+{
+    public class PlaceSearchPageViewModel : BaseViewModel
+    {
+        private ReactiveCommand _backButtonCommand;
+        public ReactiveCommand BackButtonCommand => _backButtonCommand;
+
+        private ReactiveCommand _autoCompletePlacesCommand;
+        public ReactiveCommand AutoCompletePlacesCommand => _autoCompletePlacesCommand;
+
+        private AutocompletePlace _selectInMapDefaultPlace;
+
+        #region BINDABLE PROPERTIES
+
+        private AutocompletePlace _selectedItem;
+        public AutocompletePlace SelectedItem
+        {
+            get { return _selectedItem; }
+            set { this.RaiseAndSetIfChanged(ref _selectedItem, value); }
+        }
+
+        private List<AutocompletePlace> _placesList;
+        public List<AutocompletePlace> PlacesList
+        {
+            get { return _placesList; }
+            set { this.RaiseAndSetIfChanged(ref _placesList, value); }
+        }
+        #endregion
+
+        public PlaceSearchPageViewModel()
+        {
+            _backButtonCommand = ReactiveCommand.Create(BackButtonCommandAsync);
+            _autoCompletePlacesCommand = ReactiveCommand.Create<object>(param => AutoCompletePlaces(param));
+        }
+
+        public override void Load(NavigationParameters navigationParameters)
+        {
+            base.Load(navigationParameters);
+
+            _selectInMapDefaultPlace = new AutocompletePlace()
+            {
+                ID = "select_in_map",
+                Description = "Selección en mapa"
+            };
+            PlacesList = new List<AutocompletePlace>() { _selectInMapDefaultPlace };
+        }
+
+        #region COMMAND HANDLERS
+        private void BackButtonCommandAsync()
+        {
+            this.NavigateBack(null);
+        }
+
+        private void AutoCompletePlaces(object parameter)
+        {
+            var placesList = (List<AutocompletePlace>)parameter;
+
+            if (placesList != null)
+            {
+                placesList.Insert(0, _selectInMapDefaultPlace); // Insert at 0 index Select in Map option
+                PlacesList = placesList;
+            }
+        }
+
+        public ICommand ItemTappedCommand => (new Command(
+          (object sender) =>
+          {
+              var place = ((AutocompletePlace)sender);
+
+              NavigationParameters navigatonParameters = new NavigationParameters();
+              navigatonParameters.Add("place-selected", place);
+
+              this.NavigateBack(navigatonParameters);
+          }));
+        #endregion
+    }
+}
