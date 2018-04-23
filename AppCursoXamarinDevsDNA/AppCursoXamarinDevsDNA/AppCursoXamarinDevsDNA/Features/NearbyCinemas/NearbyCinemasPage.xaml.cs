f6fb7efc564ac35711cb8103c8f66fdbe10e2cc1
Enrique <enri46@gmail.com>
2018-04-24 00:48:19 +0200

    Página NearbyCinemas

---
+++ AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/Features/NearbyCinemas/NearbyCinemasPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using AppCursoXamarinDevsDNA.Services.NavigationService;
+using ReactiveUI;
+using System;
+using Xamarin.Forms.Xaml;
+
+namespace AppCursoXamarinDevsDNA.Features.NearbyCinemas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NearbyCinemasPage
+	{
+		public NearbyCinemasPage (NavigationParameters navigationParameters = null) : base(navigationParameters)
+		{
+			InitializeComponent ();
+		}
+
+        public override void CreateBindings(Action<IDisposable> d)
+        {
+            base.CreateBindings(d);
+
+            d(this.OneWayBind(ViewModel, vm => vm.CinemasList, v => v.listViewNearbyCinemas.ItemsSource));
+            d(this.Bind(ViewModel, vm => vm.SelectedItem, v => v.listViewNearbyCinemas.SelectedItem));
+        }
+    }
+}
---
+++ AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/AppCursoXamarinDevsDNA/Features/NearbyCinemas/NearbyCinemasPageViewModel.cs
@@ -0,0 +1,81 @@
+﻿using AppCursoXamarinDevsDNA.Base;
+using AppCursoXamarinDevsDNA.Services.NavigationService;
+using AppCursoXamarinDevsDNA.Services.NearbyCinemas;
+using ReactiveUI;
+using Splat;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace AppCursoXamarinDevsDNA.Features.NearbyCinemas
+{
+    public static class NavigationKeys
+    {
+        public const string MAP_POSITION = "map-position";
+    }
+
+    public class NearbyCinemasPageViewModel : BaseViewModel
+    {
+        private readonly INearbyCinemasService _nearbyCinemasService;
+
+        #region BINDABLE PROPERTIES
+        private Cinema _selectedItem;
+        public Cinema SelectedItem
+        {
+            get { return _selectedItem; }
+            set { this.RaiseAndSetIfChanged(ref _selectedItem, value); }
+        }
+
+        private List<Cinema> _cinemasList;
+        public List<Cinema> CinemasList
+        {
+            get { return _cinemasList; }
+            set { this.RaiseAndSetIfChanged(ref _cinemasList, value); }
+        }
+        #endregion
+
+        public NearbyCinemasPageViewModel() : this(null)
+        {
+
+        }
+
+        public NearbyCinemasPageViewModel(INearbyCinemasService nearbyCinemasService)
+        {
+            _nearbyCinemasService = nearbyCinemasService ?? Locator.Current.GetService<INearbyCinemasService>();
+        }
+
+        public override async void Load(NavigationParameters navigationParameters)
+        {
+            base.Load(navigationParameters);
+
+            try
+            {
+                if (navigationParameters.ContainsKey(NavigationKeys.MAP_POSITION))
+                {
+                    Position mapPosition = (Position)navigationParameters[NavigationKeys.MAP_POSITION];
+
+                    CinemasList = await _nearbyCinemasService.GetCinemas(mapPosition);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error en Load() en NearbyCinemasPageViewModel:");
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        #region COMMANDS
+        public ICommand ItemTappedCommand => (new Command(
+          (object sender) =>
+          {
+              SelectedItem = null;
+          }));
+        #endregion
+    }
+}
